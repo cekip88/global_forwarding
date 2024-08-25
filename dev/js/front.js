6ca9c66d328342dddfbcc7b8b31e0cc5acd33e0b
cekip88 <cekip@mailr.u>
2024-08-25 15:26:04 +0500

    home page without hovers

--- dev/js/front.js
+++ dev/js/front.js
@@ -11,10 +11,69 @@ class Front extends G_G{
     const _ = this;
     _.componentName = 'front';
     G_Bus.on(_,[
-        ''
+        'mainSliderPrev',
+        'mainSliderNext',
+        'mainSliderDotClick',
     ]);
   }
+	mainSliderPrev({item,event}){
+		const _ = this;
+		let slider = item.nextElementSibling;
+		let slide = slider.querySelector('.main-slide');
+		let offset = slider.getAttribute('data-offset') ?? 0;
+		let dots = item.parentElement.lastElementChild;
 
+		let newOffset = offset * 1 - 1;
+		if (newOffset < 0) return;
+		slider.setAttribute('data-offset',newOffset.toString());
+		slide.style = `margin-left:-${newOffset * 1 * 100}%`;
+
+		let dot = dots.children[newOffset];
+		dots.querySelector('.active').classList.remove('active');
+		dot.classList.add('active');
+	}
+	mainSliderNext({item,event}){
+		const _ = this;
+		let slider = item.previousElementSibling;
+		let slide = slider.querySelector('.main-slide');
+		let offset = slider.getAttribute('data-offset') ?? 0;
+		let dots = item.parentElement.lastElementChild;
+
+		let newOffset = offset * 1 + 1;
+		if (newOffset + 1 > slider.children.length) return;
+		slider.setAttribute('data-offset',newOffset.toString());
+		slide.style = `margin-left:-${newOffset * 1 * 100}%`;
+
+		let dot = dots.children[newOffset];
+		dots.querySelector('.active').classList.remove('active');
+		dot.classList.add('active');
+	}
+	mainSliderDotClick({item,event}){
+		const _ = this;
+		let dot = event.target;
+		if (!dot.classList.contains('main-slider_dot')) return;
+
+		let activeDot = item.querySelector('.active');
+		if (activeDot === dot) return;
+
+		activeDot.classList.remove('active');
+		dot.classList.add('active');
+
+		let offset = 0;
+		for (let i = 0; i < item.children.length; i++) {
+			let children = item.children[i];
+			if (children === dot) {
+				offset = i;
+				break;
+			}
+		}
+
+		let slider = item.parentElement.querySelector('.main-slider_inner');
+		let slide = slider.querySelector('.main-slide');
+
+		slider.setAttribute('data-offset',offset.toString());
+		slide.style = `margin-left:-${offset * 1 * 100}%`;
+	}
 }
 
 new Front();
