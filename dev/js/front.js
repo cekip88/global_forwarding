d887d4f813c54623484e521f4ae71d0ac8df456d
cekip88 <cekip@mailr.u>
2024-08-26 13:29:17 +0500

    mobile header

--- dev/js/front.js
+++ dev/js/front.js
@@ -1,22 +1,79 @@
 import G_G from "./libs/G_G.js";
-import { G_Bus } from "./libs/G_Control.js";
+import {G_Bus} from "./libs/G_Control.js";
 
-class Front extends G_G{
-  constructor(){
-    super();
-    const _ = this;
-    // G_Bus
-  }
-  define(){
-    const _ = this;
-    _.componentName = 'front';
-    G_Bus.on(_,[
-        'mainSliderPrev',
-        'mainSliderNext',
-        'mainSliderDotClick',
-    ]);
-  }
-	mainSliderPrev({item,event}){
+class Front extends G_G {
+	constructor() {
+		super();
+		const _ = this;
+		// G_Bus
+	}
+
+	define() {
+		const _ = this;
+		_.componentName = 'front';
+		G_Bus.on(_, [
+			'headBurgerClick',
+			'showHeadSubmenu',
+			'mainSliderPrev',
+			'mainSliderNext',
+			'mainSliderDotClick',
+		]);
+	}
+
+	headBurgerClick({item,event}) {
+		const _ = this;
+		event.preventDefault();
+
+		let head = item.closest('.head');
+
+		if (!item.classList.contains('active')) {
+			head.classList.add('active');
+			item.classList.add('active');
+		} else {
+			head.classList.remove('active');
+			item.classList.remove('active');
+
+			head.querySelectorAll('.showed').forEach(active => {
+				_.closeHeadSubmenu({item:active.previousElementSibling})
+			})
+		}
+	}
+
+	showHeadSubmenu({item,event}) {
+		const _ = this;
+		event.preventDefault();
+
+		let list = item.nextElementSibling;
+
+		if (!item.classList.contains('active')) {
+			list.classList.add('active');
+			item.classList.add('active');
+			setTimeout(()=>{
+				list.classList.add('showed')
+			},350)
+		} else {
+			_.closeHeadSubmenu({item})
+		}
+	}
+	closeHeadSubmenu({item}) {
+		const _ = this;
+		let list = item.nextElementSibling;
+
+		list.classList.remove('showed');
+		list.querySelectorAll('.showed').forEach(active=> {
+			active.previousElementSibling.classList.remove('active');
+			active.classList.remove('showed');
+			setTimeout(()=>{
+				active.classList.remove('active');
+			})
+		})
+		setTimeout(()=>{
+			list.classList.remove('active');
+			item.classList.remove('active');
+		})
+	}
+
+	mainSliderPrev({item, event}) {
 		const _ = this;
 		let slider = item.nextElementSibling;
 		let slide = slider.querySelector('.main-slide');
@@ -25,14 +82,15 @@ class Front extends G_G{
 
 		let newOffset = offset * 1 - 1;
 		if (newOffset < 0) return;
-		slider.setAttribute('data-offset',newOffset.toString());
+		slider.setAttribute('data-offset', newOffset.toString());
 		slide.style = `margin-left:-${newOffset * 1 * 100}%`;
 
 		let dot = dots.children[newOffset];
 		dots.querySelector('.active').classList.remove('active');
 		dot.classList.add('active');
 	}
-	mainSliderNext({item,event}){
+
+	mainSliderNext({item, event}) {
 		const _ = this;
 		let slider = item.previousElementSibling;
 		let slide = slider.querySelector('.main-slide');
@@ -41,14 +99,15 @@ class Front extends G_G{
 
 		let newOffset = offset * 1 + 1;
 		if (newOffset + 1 > slider.children.length) return;
-		slider.setAttribute('data-offset',newOffset.toString());
+		slider.setAttribute('data-offset', newOffset.toString());
 		slide.style = `margin-left:-${newOffset * 1 * 100}%`;
 
 		let dot = dots.children[newOffset];
 		dots.querySelector('.active').classList.remove('active');
 		dot.classList.add('active');
 	}
-	mainSliderDotClick({item,event}){
+
+	mainSliderDotClick({item, event}) {
 		const _ = this;
 		let dot = event.target;
 		if (!dot.classList.contains('main-slider_dot')) return;
@@ -71,7 +130,7 @@ class Front extends G_G{
 		let slider = item.parentElement.querySelector('.main-slider_inner');
 		let slide = slider.querySelector('.main-slide');
 
-		slider.setAttribute('data-offset',offset.toString());
+		slider.setAttribute('data-offset', offset.toString());
 		slide.style = `margin-left:-${offset * 1 * 100}%`;
 	}
 }
